e9f42d20ee6fa9a4b8f7cce918b6ede97fd83a88
Leonard Lesinski <oxygen@leox.dev>
2026-01-20 15:47:35 +0100

    Update oneline-full.js

--- oneline-full.js
+++ oneline-full.js
@@ -2,7 +2,7 @@ function createOneLine() {
   const variables = {};
   function selfFunction(otherFunction, loopOptions) {
     for (let i = 0; loopOptions?.c?.(i) ?? i < 1; loopOptions?.p?.() ?? i++)
-      otherFunction(variables, i);
+      otherFunction(variables, i, selfFunction);
     return selfFunction;
   }
   return selfFunction;
